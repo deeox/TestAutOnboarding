ab8ff119c7c13b6ddfa19eb136c108d88bb336c9
deeox <deepakdt159@gmail.com>
2023-05-09 20:07:23 +0530

    fix flakiness

--- LocalDriverTestSuite/add_to_cart_flow_spec.rb
+++ LocalDriverTestSuite/add_to_cart_flow_spec.rb
@@ -16,11 +16,15 @@ describe 'AddToCart Steps' do
   end
 
   it 'should add product to cart' do
-    @wait.until { @driver.find_element(:css, '.shelf-item') }
+    @wait.until { @driver.find_element(:css, '.shelf-item').displayed? }
+    @wait.until { @driver.find_element(:css, '.shelf-item').displayed? }
     product_to_buy = @driver.find_element(:css, '#\31 > .shelf-item__title').text
     @wait.until { @driver.find_element(:css, '#\31 > .shelf-item__buy-btn') }.click
-    product_added_to_cart = @driver.find_element(:css, '.shelf-item__details > .title').text
-    expect(product_to_buy).to eq(product_added_to_cart)
+    product_added_to_cart = @wait.until do
+      tmp_element = @driver.find_element(:css, '.shelf-item__details > .title')
+      tmp_element if tmp_element.displayed?
+    end
+    expect(product_to_buy).to eq(product_added_to_cart.text)
     expect(@driver.find_elements(:css, '.buy-btn').length).to eq(1)
   end
 end
--- LocalDriverTestSuite/checkout_flow_spec.rb
+++ LocalDriverTestSuite/checkout_flow_spec.rb
@@ -10,9 +10,7 @@ describe 'Checkout Steps' do
     @driver.get('https://bstackdemo.com/')
     @driver.manage.window.resize_to(1920, 984)
     login_with_demouser
-    sleep 2
     add_to_cart
-    sleep 2
   end
 
   after(:each) do
@@ -20,8 +18,12 @@ describe 'Checkout Steps' do
   end
 
   it 'should checkout with valid details' do
-    @wait.until { @driver.find_element(:css, '.buy-btn') }.click
-    @wait.until { @driver.find_elements(:css, '.checkout-form') }
+    element = @wait.until do
+      tmp_element = @driver.find_element(:css, '#__next > div > div > div.float-cart.float-cart--open > div.float-cart__content > div.float-cart__footer > div.buy-btn')
+      tmp_element if tmp_element.enabled? && tmp_element.displayed?
+    end
+    element.click
+    @wait.until { @driver.find_element(:css, '*[data-test="shipping-address-heading"]').displayed? }
     @driver.find_element(:id, 'firstNameInput').send_keys('abc')
     @driver.find_element(:id, 'lastNameInput').send_keys('abc')
     @driver.find_element(:id, 'addressLine1Input').send_keys('abc')
@@ -43,12 +45,18 @@ describe 'Checkout Steps' do
   end
 
   def add_to_cart
-    @wait.until { @driver.find_element(:css, '.shelf-item') }
+    @wait.until { @driver.find_element(:css, '.shelf-item').displayed? }
+    @wait.until do
+      tmp_element = @driver.find_element(:css, '.shelf-item')
+      tmp_element if tmp_element.displayed?
+    end
     product_to_buy = @driver.find_element(:css, '#\31 > .shelf-item__title').text
     @wait.until { @driver.find_element(:css, '#\31 > .shelf-item__buy-btn') }.click
-    sleep 2
-    product_added_to_cart = @wait.until { @driver.find_element(:css, '.shelf-item__details > .title') }.text
-    expect(product_to_buy).to eq(product_added_to_cart)
+    product_added_to_cart = @wait.until do
+      tmp_element = @driver.find_element(:css, '.shelf-item__details > .title')
+      tmp_element if tmp_element.displayed?
+    end
+    expect(product_to_buy).to eq(product_added_to_cart.text)
     expect(@driver.find_elements(:css, '.buy-btn').length).to eq(1)
   end
 end
